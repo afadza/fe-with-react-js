20ebb52402e9942aa6145460c6cb106f7d272c4f
afadza <arya.skoba@gmail.com>
2023-09-21 22:39:30 +0700

    3.Props

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import User from "./Components/User";
 import Age from "./Components/Age";
 import Agree from "./Components/Agree";
+import Thankyou from "./Components/Thankyou";
 
 export default class App extends React.Component {
   render() {
@@ -11,6 +12,7 @@ export default class App extends React.Component {
           <User />
           <Age />
           <Agree />
+          <Thankyou />
         </div>
       </React.Fragment>
     );
