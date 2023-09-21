be7eb267a147af61117790550785c75cf37142fd
afadza <arya.skoba@gmail.com>
2023-09-22 03:41:20 +0700

    4.Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,53 @@
 import React from "react";
-import User from "./Components/User";
-import Age from "./Components/Age";
-import Agree from "./Components/Agree";
-import Thankyou from "./Components/Thankyou";
 
-export default class App extends React.Component {
+interface Data {
+  name: string
+  username: string
+  email: string
+}
+
+interface State {
+  data: Data[] | null
+}
+
+export default class App extends React.Component<{value: string}, State> {
+  constructor(props: {value: ''}) {
+    super(props)
+    this.state = {
+      data: null
+    }
+  }
+
+  componentDidMount(): void {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch('https://jsonplaceholder.typicode.com/users/1')
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      const dataArray = Array.isArray(data) ? data : [data]
+      this.setState({data: dataArray})
+    })
+    .catch((error) => {
+      alert(`Error fetching ${error}`)
+    })
+  }
+
   render() {
+    const {data} = this.state
     return (
       <React.Fragment>
         <div>
-          <User />
-          <Age />
-          <Agree />
-          <Thankyou />
+          {data && data.map((data) => (
+            <div>
+              <p>{data?.name}</p>
+              <p>{data?.username}</p>
+              <p>{data?.email}</p>
+            </div>
+          ))}
         </div>
       </React.Fragment>
     );
